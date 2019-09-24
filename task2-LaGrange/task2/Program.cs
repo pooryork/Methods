865f3af3c6e9005809b93050da88f0e111b9df03
Artyom Burdavitsyn <artyom.burdavitsyn@gmail.com>
2019-09-24 14:06:44 +0400

    Сданный метод Лагранжа

---
+++ task2-LaGrange/task2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] mas_x = new double[] { 1, 2, 3, 4 };
+            double[] mas_y = new double[] { 7, 28, 63, 112 };
+
+            StringBuilder first = new StringBuilder(), second = new StringBuilder();
+
+            int length = mas_x.Length - 1;
+            for (int i = 0; i < length; i++)
+            {
+                first.Append("x = " + mas_x[i] + ", ");
+                second.Append("y = " + mas_y[i] + ", L(x) = ");
+                double x = (mas_x[i] + mas_x[i + 1]) / 2;  //( x(i) + x(i+1) )/2
+                double la_Grange = 0;
+                for (int j = 0; j <= length; j++)
+                {
+                    double l = 1;
+
+                    for (int k = 0; k <= length; k++)
+                    {
+                        if (j != k)
+                        {
+                            l *= (x - mas_x[k])/(mas_x[j] - mas_x[k]);
+                        }
+                    }
+                    l *= mas_y[j];
+                    la_Grange += l;
+
+                }
+                first.Append("sub_x = " + x + " | ");
+                second.Append(System.Math.Round(la_Grange, 4) + " | ");
+
+            }
+            /*first.Append(mas_x[2] + "  |");
+            second.Append(mas_y[2] + "  |");*/
+            Console.WriteLine(first);
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.WriteLine(second);
+        }
+    }
+}
---
+++ task2-Newton/task2-Newton/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace task2_Newton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> mas_x = new List<double> { 1, 2, 3, 4 };
+            List<double> mas_y = new List<double> { 7, 28, 63, 112 };
+
+            Dictionary<int, List<double>> list = new Dictionary<int, List<double>>();
+
+            list.Add(0, mas_y);
+            int n = 1;
+            int count = mas_x.Count;
+            int h = 0;
+            for (int i = 0; i < count; i++)
+            {
+                h++;
+                List<double> lst = new List<double>();
+                for (int j = 0; j < count - n; j++)
+                {
+                    double element = (list[i][j + 1] - list[i][j]) / (mas_x[j + h] - mas_x[j]);
+                    lst.Add(System.Math.Round(element, 2));
+                }
+                list.Add(i + 1, lst);
+                n++;
+            }
+            foreach (KeyValuePair<int, List<double>> keyValue in list)
+            {
+                foreach (double element in keyValue.Value)
+                {
+                    Console.Write(String.Format("{0,6}", element));
+                }
+                Console.WriteLine();
+            }
+            double newton = mas_y[0];
+            for (int i = 0; i < count - 1; i++)
+            {
+                double x = (mas_x[i] + mas_x[i + 1]) / 2;
+                Console.Write(x + " - ");
+                Console.WriteLine(System.Math.Round(NewtonFunc(mas_x, newton, x, list, count), 4));
+            }
+        }
+        public static double NewtonFunc(List<double> mas_x, double newton, double x, Dictionary<int, List<double>> list, int count)
+        {
+            for (int i = 1; i < count; i++)
+            {
+                double func = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    func *= (x - mas_x[j]);
+                }
+                newton += (func * list[i][0]);
+            }
+            return newton;
+        }
+
+    }
+}
+
